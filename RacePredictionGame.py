1cef72be94c002f49fe02478b777f88ed5402e9c
florenceyared <florence.yared@mail.mcgill.ca>
2023-07-28 11:42:54 -0400

    remove spa from 'Enter Guess'

--- RacePredictionGame.py
+++ RacePredictionGame.py
@@ -294,7 +294,7 @@ with tabs_guess:
     users = [""]
     for i in userstouse:
         users.append(i)
-    gps = ["Spa", "Zandvoort", "Monza", "Singapore", "Japan", "Qatar", "USA", "Mexico", "Brazil", "Las Vegas", "Abu Dhabi"]
+    gps = ["Zandvoort", "Monza", "Singapore", "Japan", "Qatar", "USA", "Mexico", "Brazil", "Las Vegas", "Abu Dhabi"]
     drivers = ['VER', 'PER', 'LEC', 'SAI', 'HAM', 'RUS', 'ALO', 'STR', 'GAS', 'OCO', 'NOR', 'PIA', 'MAG', 'HUL', 'ALB', 'SAR', 'BOT', 'ZHO',  'TSU', 'RIC']
     st.header(f"Enter your guess")
     with st.form("entry_form", clear_on_submit = True):
